a9a254f7bd649c61a719f377d6c4e6416e81bb8a
watervalleyskip <sunmiao1224@gmail.com>
2026-03-05 11:31:42 +0800

    Update my_qt_test.py

--- my_qt_test.py
+++ my_qt_test.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox
 
 def on_button_click():
-    # 这就是一个“槽”：按钮被点击后要发生的事
+    # “槽”：设定按钮被点击后要发生的事
     msg = QMessageBox()
     msg.setWindowTitle("测试提醒")
     msg.setText("你好！这是一个用 Qt 做的软件窗口！")
@@ -11,21 +11,22 @@ def on_button_click():
 # 1. 创建 Qt 应用程序的环境
 app = QApplication(sys.argv)
 
-# 2. 创建一个主窗口（乐高底板）
+# 2. 创建主窗口
 window = QWidget()
 window.setWindowTitle("慧鱼科技-模拟上位机")
 window.resize(300, 200)
 
-# 3. 创建一个按钮控件（乐高积木）
+# 3. 创建一个按钮控件
 btn = QPushButton("点击测试硬件连接")
 # 将按钮的“点击信号”连接到上面的“槽”函数
 btn.clicked.connect(on_button_click) 
 
-# 4. 把按钮放到窗口里排好
+# 4. 按钮放到窗口
 layout = QVBoxLayout()
 layout.addWidget(btn)
 window.setLayout(layout)
 
 # 5. 显示窗口，并让程序一直运行
 window.show()
+
 sys.exit(app.exec_())
